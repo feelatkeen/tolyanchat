c1feb197ac0d23ba2ad71468812ecbb2bd3df4aa
Vlad <vladfox1337@yandex.ru>
2017-06-15 13:05:05 +0300

    Create index.js

--- index.js
+++ index.js
@@ -46,6 +46,13 @@ io.on('connection', function(socket){
 		console.log('мемер ушел...')
 	});
 });
-http.listen(3000, function(){
-	console.log("Слушаю вас на порту 3000");
+
+app.set('port', (process.env.PORT || 3000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
 });
